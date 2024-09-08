bc98993c2ff2401f0377ef108fdf4b7fdb7e7093
MnarMusa <mnarmusa31@.gmail.com>
2024-09-08 20:19:08 +0300

    add demo

---
+++ webStructure/Controllers/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webStructure.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult GetMovie (int id) {
+
+            //ContentResult result= new ContentResult();
+
+            //result.Content = $"Movies with id={id}";
+            //return result;
+            return Content($"Movie with id {id}","text/html");
+        }
+
+        [ActionName ("hamadaa")]
+        [HttpPost]
+        [AcceptVerbs("Get","Post")]
+        public IActionResult Index()
+        {
+            //RedirectResult redirect = new RedirectResult("https://localhost:44395/Movie/GetMovie/10");
+
+            //return redirect;
+           // return RedirectToAction (nameof(GetMovie), new {id=10});
+           return RedirectToRoute("default", new {controller= "Product",action="GetProduct",id=100});
+        }
+    }
+}
